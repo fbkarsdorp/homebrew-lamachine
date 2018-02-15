7c5edb2237f0d2743d189675b76ee8c5f59560b8
Maarten van Gompel <proycon@anaproy.nl>
2018-02-15 14:46:51 +0100

    added toad

---
+++ Formula/toad.rb
@@ -0,0 +1,26 @@
+class Toad < Formula
+    desc "Trainer of All Data - Trainer for Frog"
+    homepage "https://languagemachines.github.io/frog"
+    url "https://github.com/LanguageMachines/toad/releases/download/v0.4/toad-0.4.tar.gz"
+    sha256 "74142b89ba07f9c281ec04932a98c5d7e108222521f8a807370c5a9be279d440"
+
+    depends_on "pkg-config" => :build
+    depends_on "libxml2"
+    depends_on "icu4c"
+    depends_on "ticcutils"
+    depends_on "timbl"
+    depends_on "timblserver"
+    depends_on "mbt"
+    depends_on "frogdata"
+    depends_on "ucto"
+    depends_on "frog"
+
+    needs :cxx11
+
+    def install
+        ENV.cxx11
+        system "./configure", "--disable-dependency-tracking",
+                              "--prefix=#{prefix}"
+        system "make", "install"
+    end
+end
