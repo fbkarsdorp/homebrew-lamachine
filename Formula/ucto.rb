6ca9d85a29e613383130f9a1ff1fea9778ec895f
Folgert Karsdorp <fbkarsdorp@gmail.com>
2018-02-19 20:07:40 +0100

    clean up

--- Formula/ucto.rb
+++ Formula/ucto.rb
@@ -6,34 +6,13 @@ class Ucto < Formula
 
   option "without-check", "skip build-time checks (not recommended)"
 
-  bottle do
-    sha256 "7112464e306403c095c1bad09cb487acd81fc929ff63cbba390242594fbc2050" => :high_sierra
-    sha256 "56a6f9ffdbb9d7959c9e95771205e58e01cbfd367c3ba2ad549d39e30ed44412" => :sierra
-    sha256 "405afb2d6830686e4e5ebe75c19dd02217e50ea863add31e6d3a41cbde1b6a6b" => :el_capitan
-    sha256 "aa4f105c027da6ae3040bfc0c9894392f08de23d0d8b5dcccced9aa799e256b9" => :x86_64_linux
-  end
-
-  head do
-    url "https://github.com/LanguageMachines/ucto.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-  end
-
   depends_on "pkg-config" => :build
-  depends_on "libtextcat" 
+  depends_on "libtextcat"
   depends_on "icu4c"
+  depends_on "ticcutils"
   depends_on "libfolia"
-  depends_on "libxslt"
-  depends_on "libxml2"
-
 
   def install
-    ENV.append "CXXFLAGS", "-I#{Formula["libtextcat"].include}"
-    ENV.append "LDFLAGS", "-ltextcat"
-    
-    system "chmod +x bootstrap.sh" if build.head?
-    system "./bootstrap.sh" if build.head?
-    
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
