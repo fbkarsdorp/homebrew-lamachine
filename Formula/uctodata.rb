d21ffdc88b0362112f2b6b7a900c01129eefed49
Folgert Karsdorp <fbkarsdorp@gmail.com>
2018-02-19 22:26:49 +0100

    update

--- Formula/uctodata.rb
+++ Formula/uctodata.rb
@@ -5,11 +5,18 @@ class Uctodata < Formula
   sha256 "a6afaac1f8330df49560d3c00c670ac734ba71974b26a01a71e08b85b147b655"
 
   depends_on "pkg-config" => :build
+  depends_on "ucto"
 
   def install
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
+    system "./configure", "--prefix=#{prefix}",
                           "--disable-silent-rules"
     system "make", "install"
+  end
+
+  def caveats; <<~EOS
+    To use the uctodata with ucto without specifying a complete path to 
+    one of the configuration files, run this:
+      ln -s #{share}/ucto/* #{Formula["ucto"].opt_share}/ucto/
+    EOS
   end
 end
