9621bd5b7b6770989aea47e8dabf708da81564ca
Folgert Karsdorp <fbkarsdorp@gmail.com>
2018-02-19 23:11:41 +0100

    Some small changes to get it running

--- Formula/foliautils.rb
+++ Formula/foliautils.rb
@@ -1,22 +1,25 @@
 class Foliautils < Formula
   desc "FoLiA Utilities"
   homepage "https://languagemachines.github.io/folia"
-  url "https://github.com/LanguageMachines/foliautils/releases/download/v0.7/foliautils-0.7.tar.gz"
-  sha256 "20e8a5bc9867108740aa5d56aa91f4bb2f2b9e73724079b1005448"
+  url "https://github.com/LanguageMachines/foliautils/releases/download/v0.8/foliautils-0.8.tar.gz"
+  sha256 "2b30e9a9920ef349a6f09cf204d187297aa73d98499b7c429ba2c41b6a39f5ee"
 
   option "without-check", "skip build-time checks (not recommended)"
 
   depends_on "pkg-config" => :build
-  depends_on "icu4c"
-  depends_on "libfolia"
-  depends_on "libxml2"
   depends_on "libtextcat"
+  depends_on "icu4c"
+  depends_on "ticcutils"
+  depends_on "libfolia"
+  depends_on "ucto"
+  depends_on "libxml2"
 
   def install
+    ENV.append "CXXFLAGS", "-I#{Formula["libtextcat"].opt_include}/libtextcat/"
+    ENV.append "LDFLAGS", "-ltextcat"
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
     system "make", "install"
-    system "make", "check" if build.with? "check"
   end
 end
