a66b173e4eff2c683b43c8a9bcf27d5b657714d1
Maarten van Gompel <proycon@anaproy.nl>
2018-04-23 21:02:41 +0200

    updated timbl to 6.4.11 and uctodata to 0.6

--- Formula/timbl.rb
+++ Formula/timbl.rb
@@ -1,8 +1,8 @@
 class Timbl < Formula
   desc "Memory-based learning algorithms"
   homepage "https://languagemachines.github.io/timbl"
-  url "https://github.com/LanguageMachines/timbl/releases/download/v6.4.10/timbl-6.4.10.tar.gz"
-  sha256 "c95ad52e78136840245e7732ec6403282894ecf187b18b49a8271f34520c2dcb"
+  url "https://github.com/LanguageMachines/timbl/releases/download/v6.4.11/timbl-6.4.11.tar.gz"
+  sha256 "ab3e99f1e3339376cf9c35d832f1b1005d0c98965babf67c722af4f8ab4f34cf"
 
   depends_on "pkg-config" => :build
   depends_on "libxml2"
--- Formula/uctodata.rb
+++ Formula/uctodata.rb
@@ -1,8 +1,8 @@
 class Uctodata < Formula
   desc "Data for Unicode Tokenizer Ucto"
   homepage "https://languagemachines.github.io/ucto"
-  url "https://github.com/LanguageMachines/uctodata/releases/download/v0.5/uctodata-0.5.tar.gz"
-  sha256 "a6afaac1f8330df49560d3c00c670ac734ba71974b26a01a71e08b85b147b655"
+  url "https://github.com/LanguageMachines/uctodata/releases/download/v0.6/uctodata-0.6.tar.gz"
+  sha256 "52251482fb383452798f31172967a5f8966bd5616e037881ed318603d6310bdb"
 
   depends_on "pkg-config" => :build
   depends_on "ucto"
@@ -14,7 +14,7 @@ class Uctodata < Formula
   end
 
   def caveats; <<~EOS
-    To use the uctodata with ucto without specifying a complete path to 
+    To use the uctodata with ucto without specifying a complete path to
     one of the configuration files, run this:
       ln -s #{share}/ucto/* #{Formula["ucto"].opt_share}/ucto/
     EOS
