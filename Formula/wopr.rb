1f5475c4a25915fdaccbcb4e02567f289726dcb8
Maarten van Gompel <proycon@anaproy.nl>
2020-04-15 14:34:32 +0200

    updated wopr

--- Formula/wopr.rb
+++ Formula/wopr.rb
@@ -1,8 +1,8 @@
 class Wopr < Formula
     desc "Memory Based Word Predictor/Language Model"
     homepage "http://ilk.uvt.nl/wopr/"
-    url "https://github.com/LanguageMachines/wopr/releases/download/v1.41/wopr-1.41.tar.gz"
-    sha256 "c7eced211af80add3390280db7298f8b6f341c1cb293598391fa2aff619a86be"
+    url "https://github.com/LanguageMachines/wopr/releases/download/v1.42/wopr-1.42.tar.gz"
+    sha256 "dea3f69bf689c14c9d307004e3834e78a673985163f6b100d90364651529daf6"
 
     depends_on "pkg-config" => :build
     depends_on "automake" => :build
