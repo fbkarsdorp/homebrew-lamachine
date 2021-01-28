112d1bf87b1681f91e66f07c0bece5a268fb00bf
Maarten van Gompel <proycon@anaproy.nl>
2021-01-28 21:00:52 +0100

    fixed syntax error

--- Formula/libfolia.rb
+++ Formula/libfolia.rb
@@ -2,7 +2,7 @@ class Libfolia < Formula
   desc "XML annotation format for linguistically annotated language resources"
   homepage "https://proycon.github.io/folia/"
   url "https://github.com/LanguageMachines/libfolia/releases/download/v2.7/libfolia-2.7.tar.gz"
-  sha2.7 "98a1bf6c3a395aa7e82bd482ce7406f1451df16444d9b67c0f8ee4ac8f515ac1"
+  sha256 "98a1bf6c3a395aa7e82bd482ce7406f1451df16444d9b67c0f8ee4ac8f515ac1"
 
   option "without-check", "skip build-time checks (not recommended)"
   deprecated_option "without-check" => "without-test"
