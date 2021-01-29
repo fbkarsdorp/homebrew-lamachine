c145c93a974d65ae66b549bf10659549697d35c8
Maarten van Gompel <proycon@anaproy.nl>
2021-01-29 12:06:47 +0100

    fixed checksum

--- Formula/libfolia.rb
+++ Formula/libfolia.rb
@@ -2,7 +2,7 @@ class Libfolia < Formula
   desc "XML annotation format for linguistically annotated language resources"
   homepage "https://proycon.github.io/folia/"
   url "https://github.com/LanguageMachines/libfolia/releases/download/v2.7/libfolia-2.7.tar.gz"
-  sha256 "98a1bf6c3a395aa7e82bd482ce7406f1451df16444d9b67c0f8ee4ac8f515ac1"
+  sha256 "619779ca2f2adf0f534b5b386f3296b09be773caf055f6480f55f13071bf04a1"
 
   option "without-check", "skip build-time checks (not recommended)"
   deprecated_option "without-check" => "without-test"
