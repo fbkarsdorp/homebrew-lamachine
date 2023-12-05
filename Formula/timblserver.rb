70e872f86acc00edd9a124a35b89de9e01e2f681
Maarten van Gompel <proycon@anaproy.nl>
2023-12-05 15:51:21 +0100

    timblserver: update

--- Formula/timblserver.rb
+++ Formula/timblserver.rb
@@ -1,8 +1,8 @@
 class Timblserver < Formula
   desc "Memory-based learning algorithms"
   homepage "https://languagemachines.github.io/timbl"
-  url "https://github.com/LanguageMachines/timblserver/releases/download/v1.17/timblserver-1.17.tar.gz"
-  sha256 "8db4d234937ed4af4b92a10226d9415a65ac2dc8e9e5d7ff5f4425c8359bbedc"
+  url "https://github.com/LanguageMachines/timblserver/releases/download/v1.18/timblserver-1.18.tar.gz"
+  sha256 "8eafad2268c6db37847bde16928abfa917dd9041cf85c2847d5b82386e750c35"
 
   depends_on "pkg-config" => :build
   depends_on "timbl"
