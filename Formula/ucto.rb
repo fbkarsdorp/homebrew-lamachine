2a5c6a53a578231005448592470bd3e38e05eb4f
Maarten van Gompel <proycon@anaproy.nl>
2024-02-29 22:45:22 +0100

    ucto: update to v0.31

--- Formula/ucto.rb
+++ Formula/ucto.rb
@@ -1,8 +1,8 @@
 class Ucto < Formula
   desc "Unicode tokenizer"
   homepage "https://languagemachines.github.io/ucto"
-  url "https://github.com/LanguageMachines/ucto/releases/download/v0.30/ucto-0.30.tar.gz"
-  sha256 "de72a8ff948a271a4b1d208850a2a75812b53a9446ba57d52760f51fb059f3a2"
+  url "https://github.com/LanguageMachines/ucto/releases/download/v0.31/ucto-0.31.tar.gz"
+  sha256 "0a101b43448a27c68e08c08b934b32585b254631c38265e9fb9bbf653266fb0b"
 
   option "without-check", "skip build-time checks (not recommended)"
 
