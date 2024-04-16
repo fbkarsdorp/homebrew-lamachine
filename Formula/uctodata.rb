9bbc7523a25031a5d1f8012464cbb752f5939453
Maarten van Gompel <proycon@anaproy.nl>
2024-04-16 10:24:40 +0200

    uctodata: upgrade to v0.10.1

--- Formula/uctodata.rb
+++ Formula/uctodata.rb
@@ -1,8 +1,8 @@
 class Uctodata < Formula
   desc "Data for Unicode Tokenizer Ucto"
   homepage "https://languagemachines.github.io/ucto"
-  url "https://github.com/LanguageMachines/uctodata/releases/download/v0.9.1/uctodata-0.9.1.tar.gz"
-  sha256 "adaa4154d2b2d1d1cba108e972124a5ebd4d6b4bd3a7aec3c935af996777338d"
+  url "https://github.com/LanguageMachines/uctodata/releases/download/v0.10.1/uctodata-0.10.1.tar.gz"
+  sha256 "e0f56239b05f876b1f2107d22cc8032e682db0f1d7d998f7ace2cf65d108d632"
 
   depends_on "pkg-config" => :build
 
