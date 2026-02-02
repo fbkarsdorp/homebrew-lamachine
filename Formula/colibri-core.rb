f1b676125694d91428f1b7263618ea1e3b827ff1
Maarten van Gompel <proycon@anaproy.nl>
2026-02-02 20:16:19 +0100

    colibri-core: upgrade to v2.5.10 and re-enable test

--- Formula/colibri-core.rb
+++ Formula/colibri-core.rb
@@ -1,8 +1,8 @@
 class ColibriCore < Formula
     desc "Colibri Core"
     homepage "https://proycon.github.io/colibri-core"
-    url "https://github.com/proycon/colibri-core/archive/refs/tags/v2.5.9.tar.gz"
-    sha256 "ac39d13b2de6a550f6707a03bc1740d27a0c500833bc6f1ef5f7e7363d9fee29"
+    url "https://github.com/proycon/colibri-core/archive/refs/tags/v2.5.10.tar.gz"
+    sha256 "a37bef0cbf56e1e44d61a778f0bc656a6b951da5523dde1bfdc959feb16250a9"
 
     depends_on "pkg-config" => :build
     depends_on "autoconf" => :build
